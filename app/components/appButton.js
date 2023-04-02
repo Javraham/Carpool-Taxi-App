630608c23dbe8fd7a0f53458d514b0e293861fec
Jonathan <avrahamjonathan@gmail.com>
2023-04-02 19:01:30 -0400

    Completed Registration, Login, and Welcome page

---
+++ app/components/appButton.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+
+function AppButton({text, onPress, txtColor, bgColor}){
+    return(
+      <TouchableOpacity onPress = {onPress}>
+        <View style = {[styles.button, {backgroundColor: bgColor}]}>
+          <Text style = {[styles.buttonText, {color: txtColor}]}>{text.toUpperCase()}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+const styles = StyleSheet.create({
+    button: {
+        borderRadius: 50,
+        paddingVertical: 10,
+        alignItems: 'center',
+    },
+
+    buttonText: {
+        fontWeight: 'bold',
+    }
+})
+
+
+export default AppButton;
